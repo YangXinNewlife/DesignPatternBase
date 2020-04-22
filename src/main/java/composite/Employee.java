1e33f9aa8b86dc8b75217be7b9671de78672b416
yangxin_ryan <yangxin03@youxin.com>
2020-04-23 01:19:42 +0800

    更新

---
+++ src/main/java/composite/Employee.java
@@ -0,0 +1,38 @@
+package composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yangxin_ryan
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee employee) {
+        subordinates.add(employee);
+    }
+
+    public void remove(Employee employee) {
+        subordinates.remove(employee);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return ("Employ :[ Name : " + name + ", dept : " + dept + ", salary :" + salary + " ]");
+    }
+}
