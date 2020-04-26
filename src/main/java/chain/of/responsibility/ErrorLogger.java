55d6e61005b6c3fb535000a68d8819312d0f4177
yangxin_ryan <yangxin03@youxin.com>
2020-04-26 15:59:06 +0800

    更新

---
+++ src/main/java/chain/of/responsibility/ErrorLogger.java
@@ -0,0 +1,16 @@
+package chain.of.responsibility;
+
+/**
+ * @author yangxin_ryan
+ */
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
